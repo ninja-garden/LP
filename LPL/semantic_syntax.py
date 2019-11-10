9154fd46db295ae437f4a8cf2c869a5d39d0a7b1
alena195101 <alena195101@yandex.ru>
2019-11-10 21:44:00 +0300

    Update semantic_syntax.py

--- LPL/semantic_syntax.py
+++ LPL/semantic_syntax.py
@@ -4,6 +4,7 @@ import pymorphy2
 import re 
 #!pip install pymystem3
 #!pip install git+https://github.com/nlpub/pymystem3
+#Функция нахождения словосочетаний по ключевым словам (на входе слова по классам)
 def Collocations(sentence):
     separators = ['F','&','V','L','K']
     result = [[]]
@@ -24,8 +25,28 @@ def Collocations(sentence):
         else:
             del result[i]
     return result
-    
-    def class_word(word_list):
+#Функция определения типа словосочетания
+"""Пока тут заглушка, всегда возвращается именное"""
+def CollocationType(collocation):
+    return 'N'
+'''Пока определяет главное слово только в именном словосочетании'''
+def find_the_main_word(collocation):
+    if CollocationType(collocation) == 'N':
+        if 'N' in collocation:
+            return collocation.index('N') + 1
+        else:
+            return None
+#Функция вывлда информации о совосочетаниях (принимает на вход результат функции Collocations)
+def CollocationsInfo(collocations):
+    collocations_info = [[1,len(collocations[0])]]
+    for encoding in collocations[1:]:
+        collocations_info.append([collocations_info[-1][0]+collocations_info[-1][1],len(encoding)])
+    for i in range(len(collocations)):
+        collocations_info[i].append(CollocationType(collocations[i]))
+        collocations_info[i].append(find_the_main_word(collocations[i]))
+    return [tuple(info) for info in collocations_info]
+
+def class_word(word_list):
     dict_class = {'NOUN':'N',
                     'ADJF':'A',
                     'VERB':'V',
@@ -48,11 +69,3 @@ def Collocations(sentence):
     list_class = [dict_class.get(word) if not(str(word) in list_punctuation) and (word in dict_class.keys())
             else word  for word in sens]
     return list_class
-
-
-
-text = ['Морфологическому', 'и', 'семантико-синтаксическому', 'анализу', 'и', 'синтезу', 'была','посвящена', 'обширная', 
-'литература', '.']
-
-class_text = class_word(text)
-Collocations(class_text)
